af1581e9146e35fe9b37c6418d5194ce0f85ac1c
kwaimuk <teaelixer@gmail.com>
2017-02-24 20:05:25 -1000

    completed

--- js/app.js
+++ js/app.js
@@ -1,4 +1,38 @@
 //Secret Menu Challenge
 /*Add event listeners to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
+var mel =document.getElementsByClassName("name");
+/*console.log(mel.length);
+*/
+for(i=0;i<mel.length;i++){
+  mel[i].addEventListener("click",showMe);
+  
+}
+
+function showMe(){
+  var mel=this.querySelectorAll(".menu")[0];
 
 
+  if (mel.style.display==="block"){
+mel.style.display="none";
+}else{
+	mel.style.display="block";
+	}
+  
+}
+/*   mel.style.color="blue";*/
+
+ 
+
+
+/*
+name
+
+for (i;.length;i++){
+
+
+
+}
+if 
+else
+
+*/
